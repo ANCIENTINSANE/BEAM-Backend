a0d18862175590c57b95d9d257773e872a7f718e
Meghashyam1218 <gurrammeghashyam@outlook.com>
2023-03-22 11:51:19 +0530

    vercel test 1

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-module.exports = app;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 if (process.env.ENV === "dev") {
   app.listen(10000, () => {
     console.log("http://localhost:10000");
   });
 }
-app.get("/hy", (res, req) => {
-  res.send("hello");
-});
+module.exports = app;
