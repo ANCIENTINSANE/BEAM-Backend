e53ff2b3f196d0283f4d4a427ce7b6b86609aaa7
Meghashyam1218 <gurrammeghashyam@outlook.com>
2023-03-22 12:10:54 +0530

    deployemnt ready

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/hy", (req, res) => {
+  res.send("hello world 2");
+});
+
 if (process.env.ENV === "dev") {
   app.listen(10000, () => {
     console.log("http://localhost:10000");
